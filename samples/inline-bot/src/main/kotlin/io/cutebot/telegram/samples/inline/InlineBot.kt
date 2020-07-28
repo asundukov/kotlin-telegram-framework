ede85b96b73c052dde7d23bb35296ab24b32158f
AndreySundukov <lulonik@mail.ru>
2020-07-28 23:27:19 +0300

    Add switch inline buttons

--- samples/inline-bot/src/main/kotlin/io/cutebot/telegram/samples/inline/InlineBot.kt
+++ samples/inline-bot/src/main/kotlin/io/cutebot/telegram/samples/inline/InlineBot.kt
@@ -2,10 +2,11 @@ package io.cutebot.telegram.samples.inline
 
 
 import io.cutebot.telegram.bot.Bot
+import io.cutebot.telegram.bot.model.RawMessage
 import io.cutebot.telegram.client.model.inline.TgAnswerInlineQuery
 import io.cutebot.telegram.client.model.inline.TgInlineQuery
-import io.cutebot.telegram.client.model.inline.TgInlineQueryResult
 import io.cutebot.telegram.client.model.inline.TgInlineQueryResultArticle
+import io.cutebot.telegram.interaction.model.ChatAnswer
 
 class InlineBot(
         private val token: String
@@ -13,6 +14,10 @@ class InlineBot(
 
     override fun getToken(): String {
         return token
+    }
+
+    override fun handleMessage(message: RawMessage): ChatAnswer {
+        return ChatAnswer.inlineQueryInvitationChatSelection("Hi. Click here to try.", "try it!", "your query")
     }
 
     override fun handleInlineQuery(inlineQuery: TgInlineQuery): TgAnswerInlineQuery {
